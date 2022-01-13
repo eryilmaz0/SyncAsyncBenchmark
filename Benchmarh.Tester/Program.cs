537c7b769d0e45d0f516bb1b0629e0846be610d7
Eren YILMAZ <erenyilmazgazi@gmail.com>
2022-01-13 20:19:06 +0300

    Add project files.

---
+++ Benchmarh.Tester/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Benchmarh.Tester
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Console App Started.");
+
+            do
+            {
+                Console.WriteLine("Please Choose Request Type. (Sync = 1, Async = 2)");
+                int requestType = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("How Many Requests Do You Want to Send?");
+                int requestCount = int.Parse(Console.ReadLine());
+
+                var timer = new Stopwatch();
+
+                timer.Start();
+                var requests = requestType == 1 ? MakeSyncRequest(requestCount) : MakeAsyncRequest(requestCount);
+                await Task.WhenAll(requests);
+                timer.Stop();
+
+                Console.WriteLine($"Process Finished. Total Time : {timer.ElapsedMilliseconds} MS");
+                Console.WriteLine("If You Want To Do Another Process, Please Press r.");
+
+
+            }
+            while (Console.ReadKey().Key == ConsoleKey.R);
+        }
+
+
+
+        private static List<Task> MakeSyncRequest(int requestCount)
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://localhost:5500/api/Benchmark/") };
+            List<Task> requests = new List<Task>();
+
+
+            for(int i = 0; i < requestCount; i++)
+            {
+                requests.Add(client.GetAsync("SyncOperation"));
+            }
+
+            return requests;
+        }
+
+
+        private static List<Task> MakeAsyncRequest(int requestCount)
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://localhost:5500/api/Benchmark/") };
+            List<Task> requests = new List<Task>();
+
+
+            for (int i = 0; i < requestCount; i++)
+            {
+                requests.Add(client.GetAsync("AsyncOperation"));
+            }
+
+            return requests;
+        }
+    }
+}
---
+++ Benchmark.API/Controllers/BenchmarkController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Benchmark.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BenchmarkController : ControllerBase
+    {
+        [HttpGet]
+        [Route("SyncOperation")]
+        public IActionResult SyncOperation()
+        {
+            //We Are Simulating A Sync Process That Takees 2 seconds 
+            Thread.Sleep(2000);
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("AsyncOperation")]
+        public async Task<IActionResult> AsyncOperation()
+        {
+            //We Are Simulating An Async Process That Takees 2 seconds 
+            await Task.Delay(2000);
+            return Ok();
+        }
+    }
+}
